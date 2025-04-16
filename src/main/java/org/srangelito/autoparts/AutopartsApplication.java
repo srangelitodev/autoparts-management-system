a62deaa8c034b3e755b7ca593fc094d84ef93419
Angel Axel Leon Arreola <angel.axel.leon.arreola@gmail.com>
2025-04-16 17:24:49 -0600

    chore: Added required dependencies to the pom.xml file and configured the initial basic settings for the Spring application

---
+++ src/main/java/org/srangelito/autoparts/AutopartsApplication.java
@@ -0,0 +1,13 @@
+package org.srangelito.autoparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopartsApplication.class, args);
+	}
+
+}
